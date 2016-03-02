ced4a0a8fa68ce931dad19f2b4fd6bfa5b04884c
James Carscadden <james@carscadden.org>
2016-03-02 13:59:02 -0500

    no tty available, missing alias for link

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ docker pull postgres
 
 # Run docker containers
 docker run --name railsPostgres -e POSTGRES_PASSWORD=${1} -d postgres
-docker run -it --link railsPostgres:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "CREATE ROLE agentUsr WITH LOGIN CREATEDB PASSWORD ${2}"'
-docker run -t --name vstsagent --link railsPostgres \
+docker run -i --link railsPostgres:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "CREATE ROLE agentUsr WITH LOGIN CREATEDB PASSWORD ${2}"'
+docker run --name vstsagent --link railsPostgres:postgres \
     -e VSTS_CONFIG_USERNAME=${3} \
     -e VSTS_CONFIG_PASSWORD=${4} \
     -e VSTS_CONFIG_URL=${5} \
