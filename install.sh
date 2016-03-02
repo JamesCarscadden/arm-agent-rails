bc7281191e24cf8194876649de8ee1d59dfd04a8
James Carscadden <james@carscadden.org>
2016-03-02 16:30:42 -0500

    missing space in run command, make sure docker containers restart automatically

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ docker pull jamescarscadden/vsts-agent-rails
 docker pull postgres
 
 # Run postgres docker container
-docker run --name railsPostgres -e POSTGRES_PASSWORD=${1} -d postgres
+docker run --name railsPostgres --restart=always -e POSTGRES_PASSWORD=${1} -d postgres
 
 # Create an agent user in postgres
 pass="PASSWORD '${2}'"
@@ -17,7 +17,7 @@ conn="postgresql://postgres:${1}@railsPostgres:5432"
 docker run -i --link railsPostgres:postgres --rm postgres sh -c 'exec psql $conn -c "CREATE ROLE agentUsr WITH LOGIN CREATEDB $pass;"'
 
 # Run agent docker container
-docker run -i --name vstsagent --link railsPostgres:postgres \
+docker run -i --name vstsagent --restart=always --link railsPostgres:postgres \
     -e VSTS_CONFIG_USERNAME=${3} \
     -e VSTS_CONFIG_PASSWORD=${4} \
     -e VSTS_CONFIG_URL=${5} \
