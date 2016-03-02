9439cab476f148aef9c7be268d9dbe85c82e456f
James Carscadden <james@carscadden.org>
2016-03-02 13:43:41 -0500

    fix syntax

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-args=("$@")
-
 # Install docker and GIT
 apt-get update
 apt-get install docker.io git -y
@@ -11,11 +9,11 @@ docker pull jamescarscadden/vsts-agent-rails
 docker pull postgres
 
 # Run docker containers
-docker run --name railsPostgres -e POSTGRES_PASSWORD=${args[0]} -d postgres
-docker run -it --link railsPostgres:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "CREATE ROLE agentUsr WITH LOGIN CREATEDB PASSWORD ${args[1]}"'
+docker run --name railsPostgres -e POSTGRES_PASSWORD=${1} -d postgres
+docker run -it --link railsPostgres:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "CREATE ROLE agentUsr WITH LOGIN CREATEDB PASSWORD ${2}"'
 docker run -t --name vstsagent --link railsPostgres \
-    -e VSTS_CONFIG_USERNAME=${args[2]} \
-    -e VSTS_CONFIG_PASSWORD=${args[3]} \
-    -e VSTS_CONFIG_URL=${args[4]} \
-    -e VSTS_CONFIG_AGENTNAME=${args[5]} \
+    -e VSTS_CONFIG_USERNAME=${3} \
+    -e VSTS_CONFIG_PASSWORD=${4} \
+    -e VSTS_CONFIG_URL=${5} \
+    -e VSTS_CONFIG_AGENTNAME=${6} \
     -d jamescarscadden/vsts-agent-rails
