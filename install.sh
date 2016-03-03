0475180bb64e87dd2e5572ef1b754778def651d8
James Carscadden <james@carscadden.org>
2016-03-03 13:20:23 -0500

    Sleep for a bit after postgres docker starts , so that it's ready to go when we create the user.

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ docker pull postgres
 # Run postgres docker container
 docker run --name railsPostgres --restart=always -e POSTGRES_PASSWORD=${1} -d postgres
 
+# Wait for postgres to start up
+sleep 20
+
 # Create an agent user in postgres
 dosql="CREATE ROLE agentUsr WITH LOGIN CREATEDB PASSWORD '${2}'"
 conn="postgresql://postgres:${1}@railsPostgres:5432"
